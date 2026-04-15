3d6495190be3d6c33a2b94b23859a16038dd69d2
Sharonoito <oitosharonakoth@gmail.com>
2026-04-16 00:52:59 +0300

    productionfix2

--- next.config.js
+++ next.config.js
@@ -1,29 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-
-  serverExternalPackages: [
-    'canvas', 
-    'pdfjs-dist', 
-    'bcrypt', 
-    'xlsx',
-    '@prisma/engines'
-  ],
-
+  serverExternalPackages: ['canvas', 'pdfjs-dist'],
+  
   experimental: {
     proxyClientMaxBodySize: 50 * 1024 * 1024,
+    // This is the "Nuclear Option"
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/canvas/**',
+        'node_modules/pdfjs-dist/**',
+        '**/@swc/core-linux-x64-gnu/**',
+        '**/@swc/core-linux-x64-musl/**',
+      ],
+    },
   },
 
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'app.powerbi.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.powerbi.com',
-      },
+      { protocol: 'https', hostname: 'app.powerbi.com' },
+      { protocol: 'https', hostname: '*.powerbi.com' },
     ],
   },
 };
